f4a1ec8f92b2689ea7fd50cea01b11dc8197cf44
xenolito <oscar.rey.tajes@gmail.com>
2020-12-02 13:10:06 +0100

    minor tweaks

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function signature(config) {
       url: (config && config.url) || "https://www.pictau.com",
     };
     var args = [
-      "\n %c Made with ❤️ by " + conf.author + " %c " + conf.url + " %c %c",
+      "\n %c Made with ❤️ by " + conf.author + " %c " + conf.url + " %c %c\n",
       "border: 1px solid #000;color: #000; background: #fff001; padding:5px 0;",
       "padding:5px 0;border: 1px solid #000;",
       "color: #fff; padding:5px 0;",
