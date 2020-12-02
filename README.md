0c1f60186fb06408eb2e07164cb74ebf718393b5
xenolito <oscar.rey.tajes@gmail.com>
2020-12-02 12:48:35 +0100

    npm signature on DevTools

---
+++ README.md
@@ -0,0 +1,17 @@
+# OUTPUT AUTHOR SIGNATURE ON BROWSER DEVTOOLS
+
+Just outputs the author signature on browser's console dev tools.
+Applies formating for Chrome 38+
+
+## Install
+
+`npm i -D console-signature`
+
+## Usage
+
+just import to your project as `import signature from "console-signature"` and pass your config object with the following optional params:
+
+@param author:string --> The author
+@param url:string --> the author's web
+
+Call your function `signature({author:'author-name', url:'your-url.com'})`
---
+++ index.js
@@ -0,0 +1,22 @@
+function signature(config) {
+  if (navigator.userAgent.toLowerCase().indexOf("chrome") > -1) {
+    const conf = {
+      author: (config && config.author) || "@xenolito",
+      url: (config && config.url) || "https://www.pictau.com",
+    };
+    var args = [
+      "\n %c Made with ❤️ by " + conf.author + " %c " + conf.url + " %c %c",
+      "border: 1px solid #000;color: #000; background: #fff001; padding:5px 0;",
+      "padding:5px 0;border: 1px solid #000;",
+      "color: #fff; padding:5px 0;",
+      "color: #b0976d; padding:5px 0;",
+    ];
+    window.console.log.apply(console, args);
+  } else if (window.console) {
+    window.console.log(
+      "Made with love ❤️ by " + conf.author + " - " + conf.url
+    );
+  }
+}
+
+export default signature;
